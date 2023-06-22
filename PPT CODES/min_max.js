b1801d9eb61b519b9cff326631a87be00461d535
Amil Ali <66510886+amilali@users.noreply.github.com>
2023-06-22 12:46:58 +0530

    Update min_max.js

--- PPT CODES/min_max.js
+++ PPT CODES/min_max.js
@@ -1,37 +1,26 @@
-// brute force approach
+// find largest and smallest element in array
 
-// function sec_largest(arr){
-//     let max = Number.MIN_VALUE;
-//     let sec_max = Number.MIN_VALUE;
+large_small =(arr)=>{
+let minmax = new Array();
+let max = Number.MIN_VALUE;
+let min = Number.MAX_VALUE;
 
-//     for(let i in arr)
-//     {
-//         if(arr[i]>max)
-//         {
-//             max= arr[i];
-//         }
-//     }
-//     for(let j in arr)
-//     {
-//         if(arr[j]!=max && arr[j]>sec_max)
-//             {
-//                 sec_max = arr[j];
-//             }
-//     }
-//     return sec_max;
-// }
+arr.forEach((i) => {
+    if(i>max)
+    {
+        max = i;
+    }
+    if(i<min)
+    {
+        min = i;
+    }
 
-// optimize approach
-
-// sec_largest = (arr) =>
-// {
-//         arr = [...new Set(arr)].sort();
-//         return arr[arr.length-2];
-// }
-
-
+});
+minmax.push(max);
+minmax.push(min);
+return minmax;
+}
 
 
 let arr = [1,2,3,4,5,6,7,7];
-
-console.log(sec_largest(arr));
+console.log(large_small(arr));
