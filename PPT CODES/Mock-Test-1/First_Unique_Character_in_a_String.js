2e658030cbdf320056dc42da33f7c5c859f02a3f
Amil Ali <amil13985@gmail.com>
2023-06-25 13:52:45 +0530

    add

--- PPT CODES/Mock-Test-1/First_Unique_Character_in_a_String.js
+++ PPT CODES/Mock-Test-1/First_Unique_Character_in_a_String.js
@@ -1,43 +1,24 @@
-// var firstUniqChar = function(s) {
-//     for(let i in s)
-//     {
-//         let c=0;
-//         for(j in s)
-//         {
-//             if(s[i]==s[j])
-//             {
-//                 c++;
-//             }
-//         }
-//         if(c==1)
-//         {
-//             return i;
-//         }
-//     }
-//     return -1;
-// };
+// brute force
 
-firstUniqChar=(str)=>{
-    let mp ={};
-    str=str.split("");
-    for(let i in str)
+var firstUniqChar = function(s) {
+    for(let i in s)
     {
-        if(str[i] in mp)
-        [
-            mp[str[i]] = 2
-        ]
-        else{
-            mp[str[i]] = 1
+        let c=0;
+        for(j in s)
+        {
+            if(s[i]==s[j])
+            {
+                c++;
+            }
         }
-    }
-    for(let i in str){
-        if((str[i] in mp) ==1)
+        if(c==1)
         {
             return i;
         }
     }
     return -1;
-}
+};
+
 // optimise
 
 var firstUniqChar = function(str) {
@@ -62,6 +43,6 @@ var firstUniqChar = function(str) {
      return -1;
  };
 
- 
+
 let string ="leet code";
 console.log(firstUniqChar(string));
