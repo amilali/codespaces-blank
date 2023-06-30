cf9ea6ac446885fc75b1b82784f446c32c856a79
Amil Ali <amil13985@gmail.com>
2023-06-30 22:22:13 +0530

    add

--- Notes/head_tail.js
+++ Notes/head_tail.js
@@ -1,3 +1,9 @@
 let guess = Math.random().toString(10).slice(2,4);
 
 guess > 50 ? console.log("Head"): console.log("tail");
+
+
+// here toString take 10 -> Decimal 16-> hexadecimal
+// and 01 for -> binary value 
+
+// slice() in js work with both array and string 
