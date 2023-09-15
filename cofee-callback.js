a27875bf18315f89e528cc85d0e6c061fc907ac2
Amil Ali <66510886+amilali@users.noreply.github.com>
2023-09-15 13:30:41 +0530

    Update cofee-callback.js

--- cofee-callback.js
+++ cofee-callback.js
@@ -15,3 +15,23 @@ makingcoffee=(name)=>{
 }
 
 makeacoffee('Amil', makingcoffee);
+
+
+
+// Or you can also write this
+
+
+
+makeacoffee=(name, callback)=>
+{
+    setTimeout(() => {
+        callback(name);
+    }, 4000);
+    console.log(`${name} your coffee is making`);
+}
+
+// callback fn
+makeacoffee('Amil', (name)=>{
+    console.log(`Yay! ${name} your coffee is maked`);
+}
+);
